6ddd075cbf04f7d412d68642d51c0a395d9ad660
German Zhyvotnikov <german.zhivotnikov@gmail.com>
2024-07-28 19:41:41 +0300

    fix: some build time warnings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import 'zone.js';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
 
 if (environment.production) {
   enableProdMode();
