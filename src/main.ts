e0a26b0c52a69601aec7a51de794d371bd255e0a
German Zhyvotnikov <german.zhivotnikov@gmail.com>
2024-12-23 21:15:21 +0200

    added @angular/localize

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+/// <reference types="@angular/localize" />
+
 import 'zone.js';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
